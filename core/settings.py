d4f85d399ba444cd7cafe0383bcc3c638e0e76f4
Shin Saebyeok <toqur1219@naver.com>
2023-12-02 18:04:39 +0900

    feat:sqlite3

--- core/settings.py
+++ core/settings.py
@@ -106,25 +106,26 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'FullAuth_API',
-        'USER': 'master',
-        'PASSWORD': 'toqur9393!!',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'FullAuth_API',
+#         'USER': 'master',
+#         'PASSWORD': 'toqur9393!!',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
-# }  
+# }
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}  
     
 
 # db_from_env = dj_database_url.config(conn_max_age=500)
