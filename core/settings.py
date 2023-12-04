c77f05b0a349a8abc5033f200d4a194fd82fca21
Shin Saebyeok <toqur1219@naver.com>
2023-12-04 17:42:09 +0900

    feat:postgres direct

--- core/settings.py
+++ core/settings.py
@@ -106,34 +106,25 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'FullAuth_API',
-#         'USER': 'master',
-#         'PASSWORD': 'toqur9393!!',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-
-
-
-
-# if DEVELOPMENT_MODE is True:
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'FullAuth_API',
+        'USER': 'master',
+        'PASSWORD': 'toqur9393!!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}  
-# elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-#     if env('DATABASE_URL', None) is None:
-#         raise Exception('RDS_DB_NAME not found in os.environ')
-#     DATABASES = {
-#         'default': dj_database_url.parse(env('DATABASE_URL'))
-#         }
+}
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }  
     
 
 db_from_env = dj_database_url.config(conn_max_age=500)
