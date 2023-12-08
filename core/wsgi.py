7009613646555075147c5dfea0f755c3fb6695d7
Shin Saebyeok <toqur1219@naver.com>
2023-12-08 16:37:09 +0900

    feat:fixing wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
